dd7d3eba8eeea7412438e567a21933dd857a5795
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-05-03 09:58:58 +0530

    Add project files.

---
+++ LPRSystem.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LPRSystem.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
