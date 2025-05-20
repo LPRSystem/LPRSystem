ef965fbd6b3200c86270fd6149c4a413700c9b87
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-05-20 17:51:40 +0530

    new layout

--- LPRSystem.Web.UI/wwwroot/js/UserController.js
+++ LPRSystem.Web.UI/wwwroot/js/UserController.js
@@ -1,66 +1,65 @@
 ﻿function UserController() {
     var self = this;
     self.coreDBRoles = [];
-    self.ApplicationUser = [];
+    self.ApplicationUser = {};
     var actions = [];
-    var dataObject = [];
-    //actions.push('/Role/FetchRoles');
-
+    var dataObjects = [];
+    actions.push('/Role/FetchRoles');
     self.init = function () {
-        //var appuser = storageService.get("ApplicationUser");
-        //self.ApplicationUser = appuser;
-
+        var appuser = storageService.get("ApplicationUser");
+        if (appuser) {
+            self.ApplicationUser = appuser;
+        }
         self.usersgrid = new Tabulator("#usersgrid", {
             ajaxURL: '/User/FetchUsers',
-            ajaxParams: {},
+            ajaxParams: {  },
             ajaxConfig: {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
                 },
             },
-
             ajaxResponse: function (url, params, response) {
-                var userData = [];
-
-                userData = response.data;
-
-                return userData;
+                return response.data;
             },
             height: "600px",
             layout: "fitColumns",
             resizableColumnFit: true,
             columns: [
-                { title: "FirstName", field: "FirstName" },
-                { title: "LastName", field: "LastName" },
+                { title: "First Name", field: "FirstName" },
+                { title: "Last Name", field: "LastName" },
                 { title: "Email", field: "Email" },
                 { title: "Phone", field: "Phone" },
-                { title: "RoleId", field: "RoleId" },
-                { title: "IsActive", field: "IsActive" },
+                { title: "Role", field: "RoleId" },
+                {
+                    title: "Is Blocked",
+                    field: "IsBlocked",
+                    formatter: "tickCross",
+                    align: "center"
+                },
+                {
+                    title: "Is Active",
+                    field: "IsActive",
+                    formatter: "tickCross",
+                    align: "center"
+                }
             ]
         });
-
         //var requests = actions.map((action, index) => {
         //    var ajaxConfig = {
         //        url: action,
         //        method: 'GET'
         //    };
-        //    if (index === 2) {
-        //        ajaxConfig.data = dataObjects[0];
-        //    }
         //    return $.ajax(ajaxConfig);
         //});
 
         //$.when.apply($, requests).done(function (...responses) {
-        //    self.coreDBRoles = responses[0][0]?.data || [];
-        //    self.coreDBDealers = responses[2][0]?.data || [];
-
-        //    var loggedInUserRole = self.coreDBRoles.find(role => role.Id === self.ApplicationUser.RoleId);
+        //    self.coreDBRoles = responses[0]?.data || [];
+        //    var loggedInUserRole = self.coreDBRoles.find(role => role.RoleId === self.ApplicationUser.RoleId);
         //    if (loggedInUserRole) {
         //        var userRoles = self.filterRoles(loggedInUserRole.Name, self.coreDBRoles);
-        //        genarateDropdown("RoleId", userRoles, "Id", "Name");
+        //        genarateDropdown("RoleId", userRoles, "RoleId", "Name");
         //    }
-
         //    hideLoader();
         //}).fail(function () {
         //    console.log('One or more requests failed.');
@@ -70,13 +69,11 @@
             if (loggedInRole === 'Administrator') {
                 return roles;
             }
-            if (loggedInRole === 'System Administrator') {
-                return roles.filter(role => !['Administrator', 'System Administrator'].includes(role.Name));
-            }
-
             return [];
         };
+
         makeFormGeneric('#AddEditUserForm', '#btnsubmit');
+
 
         $(document).on("click", "#addBtn", function () {
             $('#sidebar').addClass('show');
@@ -87,7 +84,17 @@
             $('#sidebar').removeClass('show');
             $('.modal-backdrop').remove();
         });
-
+        $('#AddEditUserForm').on('submit', function (e) {
+            e.preventDefault();
+            showLoader();
+            var formData = getFormData('#AddEditUserForm');
+            var userRegistration = addCommonProperties(formData);
+            userRegistration.LastPasswordChangedOn = new Date();
+            userRegistration.IsBlocked = false;
+            userRegistration.Id = null;
+            console.log(userRegistration);
+            self.addeditUser(userRegistration);
+        });
         self.addeditUser = function (userRegistration) {
             makeAjaxRequest({
                 url: '/User/InsertOrUpdateUser',
@@ -108,5 +115,7 @@
                 }
             });
         };
-    }
+
+    };
+
 }
