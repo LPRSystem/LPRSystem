050bf9294f990a4d52d5f4245749de573cf4ec9a
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-05-26 16:29:54 +0530

    commit

--- LPRSystem.Web.API.Manager/Services/Organization/GetOrganizationsManager.cs
+++ LPRSystem.Web.API.Manager/Services/Organization/GetOrganizationsManager.cs
@@ -4,7 +4,7 @@ namespace LPRSystem.Web.API.Manager.Services.Organization
     public class GetOrganizationsDataManager : IGetOrganizationsManager
     {
         private static IGetOrganizationsRepository _repository;
-        public GetOrganizationsDataManager(IGetOrganizationsRepository repository) { _repository = repository }
+        public GetOrganizationsDataManager(IGetOrganizationsRepository repository) { _repository = repository; }
         public async Task<IEnumerable<Models.Organization.Organization>> ExecuteAsync()
         {
             return await _repository.ExecuteAsync();
