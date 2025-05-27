4b2301d35e2c4ea771c95693bf5182b229f0b377
Surbhi Jambhulkar <Surbhi.Jambhulkar@betalen.in>
2025-05-27 07:23:33 +0530

    comit insert or update user

---
+++ LPRSystem.Web.API.Manager/Services/User/IInsertOrUpdateUserManager.cs
@@ -0,0 +1,16 @@
+﻿using LPRSystem.Web.API.Manager.Models.Role;
+using LPRSystem.Web.API.Manager.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPRSystem.Web.API.Manager.Services.User
+{
+    public interface IInsertOrUpdateUserManager
+    {
+        Task<IEnumerable<LPRSystem.Web.API.Manager.Models.User.User>> ExecuteAsync(ProcessUserRequest user);
+
+    }
+}
---
+++ LPRSystem.Web.API.Manager/Services/User/InsertOrUpdateUserManager.cs
@@ -0,0 +1,27 @@
+﻿using LPRSystem.Web.API.Manager.Constants;
+using LPRSystem.Web.API.Manager.Models.Role;
+using LPRSystem.Web.API.Manager.Models.User;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPRSystem.Web.API.Manager.Services.User
+{
+    public class InsertOrUpdateUserManager : BaseRepository, IInsertOrUpdateUserManager
+    {
+        public InsertOrUpdateUserManager(IConfiguration configuration) : base (configuration) 
+        {
+            
+        }
+
+        public async Task<IEnumerable<Models.User.User>> ExecuteAsync(ProcessUserRequest request)
+        {
+            return await base.QueryAsync<Models.User.User>(CommonConstants.CommonDB, UserConstants.InsertOrUpdateUser, new { firstName = request.FirstName,lastName= request.LastName, email = request.Email, phone = request.Phone, roleId = request.RoleId, password = request.Password, passwordLastChangeOn = request.PasswordlastChangedOn, isBlocked = request.IsBlocked,  createdBy = request.CreatedBy, createdOn = request.CreatedOn, modifiedBy = request.ModifiedBy, modifiedOn = request.ModifiedOn, isActive = request.IsActive }, null);
+        }
+
+    }
+}
