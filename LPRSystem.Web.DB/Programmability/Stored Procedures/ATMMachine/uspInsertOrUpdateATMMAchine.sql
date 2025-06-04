d4cfb36b4dcefb6d63becd31b1ae88758248a44e
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-06-04 12:02:48 +0530

    comiir

--- LPRSystem.Web.DB/Programmability/Stored Procedures/ATMMachine/uspInsertOrUpdateATMMAchine.sql
+++ LPRSystem.Web.DB/Programmability/Stored Procedures/ATMMachine/uspInsertOrUpdateATMMAchine.sql
@@ -1,98 +1,87 @@
 ﻿CREATE PROCEDURE [api].[uspInsertOrUpdateATMMAchine]
 (
-	@ATMMachine [api].[ATMMAchine] Readonly
+    @ATMMachine [api].[ATMMAchine] READONLY
 )
 WITH RECOMPILE
-
 AS
-
 BEGIN
+    DECLARE @CurrentDate DATETIMEOFFSET;
+    DECLARE @CurrentUser  BIGINT;
+    DECLARE @ReturnData TABLE (
+        [ATMId] BIGINT,
+        [ATMCode] NVARCHAR(50),
+        [LocationId] NVARCHAR(50),
+        [CreatedBy] BIGINT,
+        [CreatedOn] DATETIMEOFFSET,
+        [ModifiedBy] BIGINT,
+        [ModifiedOn] DATETIMEOFFSET,
+        [IsActive] BIT
+    );
 
-DECLARE	@CurrentDate datetimeoffset;
-DECLARE	@CurrentUser bigint;
-DECLARE @RetrunData [api].[ATMMAchin];
+    SET @CurrentDate = GETDATE();
 
-set @CurrentDate = GETDATE();
+    SELECT @CurrentUser  = ModifiedBy FROM @ATMMachine;
 
-Select @CurrentUser= ModifiedBy from  @ATMMachine;
+    MERGE INTO [data].[ATMMachine] AS Target
+    USING (
+        SELECT 
+            input.[ATMId],
+            input.[ATMCode],
+            input.[LocationId],
+            input.[CreatedBy],
+            input.[CreatedOn],
+            input.[ModifiedBy],
+            input.[ModifiedOn],
+            input.[IsActive]
+        FROM @ATMMachine AS input
+        LEFT JOIN [data].[ATMMachine] AS cur ON input.ATMId = cur.ATMId
+    ) AS source
+    ON Target.ATMId = source.ATMId
+    WHEN MATCHED THEN
+        UPDATE SET 
+            [ATMCode] = source.[ATMCode],
+            [LocationId] = source.[LocationId],
+            [ModifiedBy] = @CurrentUser ,
+            [ModifiedOn] = @CurrentDate,
+            [IsActive] = source.[IsActive]
+    WHEN NOT MATCHED BY TARGET THEN 
+        INSERT (
+            [ATMCode],
+            [LocationId],
+            [CreatedBy],
+            [CreatedOn],
+            [ModifiedBy],
+            [ModifiedOn],
+            [IsActive]
+        )
+        VALUES (
+            source.[ATMCode],
+            source.[LocationId],
+            source.[CreatedBy],
+            source.[CreatedOn],
+            source.[ModifiedBy],
+            source.[ModifiedOn],
+            source.[IsActive]
+        )
+    OUTPUT 
+        inserted.[ATMId],
+        inserted.[ATMCode],
+        inserted.[LocationId],
+        inserted.[CreatedBy],
+        inserted.[CreatedOn],
+        inserted.[ModifiedBy],
+        inserted.[ModifiedOn],
+        inserted.[IsActive]
+    INTO @ReturnData;
 
-Merge INTO [data].[ATMMachine] AS Target
-
-Using (
-       select input.[ATMId]
-             ,input.[ATMCode]
-             ,input.[LocationId]
-             ,input.[CreatedBy]
-             ,input.[CreatedOn]
-             ,input.[ModifiedBy]
-             ,input.[ModifiedOn]
-             ,input.[IsActive]
-             FROM @ATMMachine input
-             LEFT JOIN 
-             [data].[ATMMachine] cur
-             on input.ATMId = cur.ATMId
-             ) as source
-             ON target.ATMId= source.ATMId
-             WHEN MATCHED THEN
-             UPDATE 
-             SET 
-             [ATMCode]         = source.[ATMCode]
-            ,[LocationId]      = source.[LocationId]
-            ,[ModifiedBy]   = @CurrentUser
-            ,[ModifiedOn]   = @CurrentDate
-            ,[IsActive]     = source.[IsActive]
-           WHEN NOT MATCHED BY TARGET THEN 
-           INSERT 
-           (
-            [ATMCode]
-           ,[LocationId]
-           ,[CreatedBy]
-           ,[CreatedOn]
-           ,[ModifiedBy]
-           ,[ModifiedOn]
-           ,[IsActive])
-           values
-           (
-            source.[ATMCode]
-           ,source.[LocationId]
-           ,source.[CreatedBy]
-           ,source.[CreatedOn]
-           ,source.[ModifiedBy]
-           ,source.[ModifiedOn]
-           ,source.[IsActive])
-           OUTPUT 
-            inserted.[ATMId]
-           ,inserted.[ATMCode]
-           ,inserted.[LocationId]
-           ,inserted.[CreatedBy]
-           ,inserted.[CreatedOn]
-           ,inserted.[ModifiedBy]
-           ,inserted.[ModifiedOn]
-           ,inserted.[IsActive]
-           INTO 
-           @RetrunData(
-            [ATMId]
-           ,[ATMCode]
-           ,[LocationId]
-           ,[CreatedBy]
-           ,[CreatedOn]
-           ,[ModifiedBy]
-           ,[ModifiedOn]
-           ,[IsActive]);
-
-  SELECT 
-            [ATMId]
-           ,[ATMCode]
-           ,[LocationId]
-           ,[CreatedBy]
-           ,[CreatedOn]
-           ,[ModifiedBy]
-           ,[ModifiedOn]
-           ,[IsActive]
-           FROM 
-            
-            @RetrunData
- END
-
-
-
+    SELECT 
+        [ATMId],
+        [ATMCode],
+        [LocationId],
+        [CreatedBy],
+        [CreatedOn],
+        [ModifiedBy],
+        [ModifiedOn],
+        [IsActive]
+    FROM @ReturnData;
+END
