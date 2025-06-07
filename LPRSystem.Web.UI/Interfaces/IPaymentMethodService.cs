e4016dc9d3384b47e78aabbbde8b73bf085eb8bc
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-06-07 11:21:36 +0530

    commit

--- LPRSystem.Web.UI/Interfaces/IPaymentMethodService.cs
+++ LPRSystem.Web.UI/Interfaces/IPaymentMethodService.cs
@@ -5,9 +5,11 @@ namespace LPRSystem.Web.UI.Interfaces
     public interface IPaymentMethodService
     {
         Task<List<PaymentMethod>> GetPaymentMethodsAsync();
+        Task<PaymentMethod> GetPaymentMethodByIdAsync(long paymentmethodid);
 
         Task<PaymentMethod> InsertOrUpdatePaymentMethodAsync(PaymentMethod paymentMethod);
 
+        Task<PaymentMethod> UpdatePaymentMethodAsync(PaymentMethod paymentMethod);
         Task<bool> DeletePaymentmethodAsync(long id);
     }
 }
