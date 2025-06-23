fba2d1954b75cdf79a9b24e58c797426fd93a4d6
T ANIL KUMAR <athatipamula@betalen.in>
2025-06-23 19:01:08 +0530

    ParkingPrice API Created

---
+++ LPRSystem.Web.API.Manager/Converters/ParkingPriceConverter.cs
@@ -0,0 +1,75 @@
+﻿using LPRSystem.Web.API.Manager.Models.ParkingPrice;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPRSystem.Web.API.Manager.Converters
+{
+    public static class ParkingPriceConverter
+    {
+        public static ParkingPrice ToSingleParkingPrice(this IDataReader reader)
+        {
+            ParkingPrice result = null;
+
+            if (reader.Read())
+            {
+                result = new ParkingPrice();
+                result.ParkingPriceId = reader.GetInt64(reader.GetOrdinal("ParkingPriceId"));
+
+                result.Duration = reader.GetString(reader.GetOrdinal("Duration"));
+
+                result.Price = reader.GetDecimal(reader.GetOrdinal("Price"));
+
+                result.CreatedBy = reader.GetInt64(reader.GetOrdinal("CreatedBy"));
+
+                if (reader.IsSafe(reader.GetOrdinal("CreatedOn")))
+                    result.CreatedOn = reader.GetDateTime(reader.GetOrdinal("CreatedOn"));
+
+                result.ModifiedBy = reader.GetInt64(reader.GetOrdinal("ModifiedBy"));
+
+                if (reader.IsSafe(reader.GetOrdinal("ModifiedOn")))
+
+                    result.ModifiedOn = reader.GetDateTime(reader.GetOrdinal("ModifiedOn"));
+
+                object isActiveValue = reader["IsActive"];
+
+                result.IsActive = (isActiveValue != DBNull.Value && isActiveValue == "1") ? true : false;
+            }
+            return result;
+        }
+
+        public static DataTable ToDataTable(this ParkingPrice source, string userId)
+        {
+            var dt = new DataTable();
+
+            //Define the columns based on the sql table structure
+
+            dt.Columns.Add("ParkingPriceId", typeof(long));
+            dt.Columns.Add("Duration", typeof(string));
+            dt.Columns.Add("Price", typeof(decimal));
+            dt.Columns.Add("CreatedBy", typeof(long));
+            dt.Columns.Add("CreatedOn", typeof(string));
+            dt.Columns.Add("ModifiedBy", typeof(long));
+            dt.Columns.Add("ModifiedOn", typeof(string));
+            dt.Columns.Add("IsActive", typeof(bool));
+
+            //Add a new row with the values from the payment object and additional fields
+
+            dt.Rows.Add(
+                source.ParkingPriceId,
+                source.Duration,
+                source.Price,
+                long.Parse(userId),
+                DateTimeOffset.UtcNow,
+                long.Parse(userId),
+                DateTimeOffset.UtcNow,
+                true
+            );
+
+            return dt;
+        }
+    }
+}
---
+++ LPRSystem.Web.Service/Functions/ParkingPrice/GetParkingPriceByIdFunction.cs
@@ -0,0 +1,73 @@
+using LPRSystem.Web.API.Manager.Models.PaymentMethod;
+using LPRSystem.Web.API.Manager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LPRSystem.Web.Service.Functions.ParkingPrice;
+
+public class GetParkingPriceByIdFunction
+{
+    private readonly ILogger<GetParkingPriceByIdFunction> _logger;
+
+    public GetParkingPriceByIdFunction(ILogger<GetParkingPriceByIdFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GetParkingPriceByIdFunction")]
+    public IActionResult GetParkingPriceById([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="parkingprice/getparkingpricebyid{parkingpriceid}")] HttpRequest req, long parkingpriceid)
+    {
+        _logger.LogInformation("GetParkingPriceById Function Invoked()");
+
+        try
+        {
+            if (parkingpriceid == 0)
+                return new BadRequestObjectResult("Please send valid payment id");
+
+            string connectionString = Environment.GetEnvironmentVariable(Global.CommonSQLServerConnectionStringSetting);
+
+            LPRSystem.Web.API.Manager.Models.ParkingPrice.ParkingPrice parkingPrice = new LPRSystem.Web.API.Manager.Models.ParkingPrice.ParkingPrice();
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("[api].[uspGetParkingPriceById]", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@parkingpriceid", parkingpriceid);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                parkingPrice.ParkingPriceId = reader.GetInt64(reader.GetOrdinal("ParkingPriceId"));
+
+                parkingPrice.Duration = reader.SafeGetString(reader.GetOrdinal("Duration"));
+
+                parkingPrice.Price = reader.GetDecimal(reader.GetOrdinal("Price"));
+
+                parkingPrice.CreatedBy = reader.GetInt64(reader.GetOrdinal("CreatedBy"));
+
+                if (reader.IsSafe(reader.GetOrdinal("CreatedOn")))
+                    parkingPrice.CreatedOn = reader.GetDateTimeOffset(reader.GetOrdinal("CreatedOn"));
+
+                parkingPrice.ModifiedBy = reader.GetInt64(reader.GetOrdinal("ModifiedBy"));
+
+                if (reader.IsSafe(reader.GetOrdinal("ModifiedOn")))
+                    parkingPrice.ModifiedOn = reader.GetDateTimeOffset(reader.GetOrdinal("ModifiedOn"));
+
+                object isActiveValue = reader["IsActive"];
+
+                parkingPrice.IsActive = (isActiveValue != DBNull.Value && isActiveValue == "1") ? true : false;
+            }
+            connection.Close();
+
+            return new OkObjectResult(parkingPrice);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
